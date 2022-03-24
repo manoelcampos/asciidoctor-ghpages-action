a007245ce6b544fb24f2f0367d1dce13660c913d
Benno Bielmeier <github@bbenno.com>
2022-03-25 00:39:49 +0100

    Fix symlinking README.html -> index.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,7 @@ eval "$INPUT_PRE_BUILD"
 if [[ $INPUT_SLIDES_SKIP_ASCIIDOCTOR_BUILD == false ]]; then
     echo "Converting AsciiDoc files to HTML"
     find . -name "*$INPUT_ADOC_FILE_EXT" -exec asciidoctor -b html $INPUT_ASCIIDOCTOR_PARAMS {} \;
-    find . -name "README.html" -exec ln -s "README.html" "$(dirname {})/index.html" \;
+    find . -name "README.html" -execdir ln -s "README.html" "index.html" \;
     find . -name "*$INPUT_ADOC_FILE_EXT" -exec git rm -f --cached {} \;
 fi
 
