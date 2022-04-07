12bf1c545088eae5e0b9c3b375765b0990629fb8
Manoel Campos <manoelcampos@gmail.com>
2022-04-07 14:59:05 -0300

    Fix #27

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ if [[ $HAS_SOURCE_DIR == true ]]; then
   git checkout -q --orphan gh-pages "$COMMIT_HASH" 1>/dev/null
   mv "$INPUT_SOURCE_DIR" /tmp/source
   #Ignores directories . and .git
-  find . -not -path './.git*' -not -name '.' -exec rm -rf {} \;
+  find . -not -path './.git*' -not -name '.' -exec rm -rf {} \; || true
   mv /tmp/source/* .
   git add .
 else
